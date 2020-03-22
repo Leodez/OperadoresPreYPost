6f9b29808204a28edfc0ec3745c5be27475ddab8
Leonardo Hernández <hernandez.leonardo@outlook.es>
2020-03-21 22:32:32 -0300

    Se agregó descriptivo inicial.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,18 @@ namespace OperadoresPreYPost
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ejemplo simple de pre-incremento y post-incremento");
+            Console.WriteLine("Inizializamos a = 3, b = ++a");
             int a = 3;
             int b = ++a;
+            Console.WriteLine("Resultado:");
             Console.WriteLine($"a = {a}");
             Console.WriteLine($"b = {b}");
             Console.WriteLine();
+
+            Console.WriteLine("Inizializamos c = 3, d = c++");
             int c = 3;
             int d = c++;
+            Console.WriteLine("Resultado:");
             Console.WriteLine($"c = {c}");
             Console.WriteLine($"d = {d}");
         }
