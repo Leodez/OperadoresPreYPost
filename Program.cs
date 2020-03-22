c88242f5171bcaedea85bf0c4f0c54c2ea4e4689
Leonardo Hernández <hernandez.leonardo@outlook.es>
2020-03-21 22:04:04 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresPreYPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = ++a;
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine();
+            int c = 3;
+            int d = c++;
+            Console.WriteLine($"c = {c}");
+            Console.WriteLine($"d = {d}");
+        }
+    }
+}
